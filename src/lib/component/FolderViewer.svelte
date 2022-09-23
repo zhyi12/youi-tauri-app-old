47bcf5b7de791b6fdf1601e5a1352b1718623f89
zhouyi <zhouyi>
2022-09-23 11:50:04 +0800

    feat: 实现资源管理器项目重命名

--- src/lib/component/FolderViewer.svelte
+++ src/lib/component/FolderViewer.svelte
@@ -2,8 +2,9 @@
 
     import type {Metadata} from "../tauri/tauri.fs";
     import {goto} from "$app/navigation";
-    import {Toolbar,Icon,folderIcon,listIcon,classnames} from "../youi";
+    import {Input,Toolbar,Icon,folderIcon,listIcon,ContextMenu,classnames} from "../youi";
     import {isNull} from "../youi/util/utils";
+    import {parseFileExtension,trimFileExtension} from "../util/filename.util";
     import {createEventDispatcher} from "svelte";
 
     const dispatch = createEventDispatcher();
@@ -11,27 +12,33 @@
     let className = '';
     export { className as class };
 
+    export let icons = ()=>undefined;
     export let rootText = "根目录";
     export let view = "flat";//flat|list
     export let folder = '';//文件夹相对路径
     export let folderUri = '';//文件夹访问路径
     export let selectedIds:Array<string> = [];//选择的节点
     export let metadatas:Array<Metadata> = [];
+    export let renamingFile:string = undefined;//重命名中的项
+
+    let container:HTMLElement = undefined;
 
     $: rootUri = folderUri.substring(0,folderUri.length - folder.length);
     $: paths = folder.split('/').filter((path)=>!isNull(path));
 
     $: classes = classnames("folder-viewer",'viewer-'+view, className);
 
-    const clickItem = (item:Metadata) => {
+    const selectItem = (item:Metadata) => {
         selectedIds = [item.name];
+        dispatch('item-select',{...item,folder,extension:parseFileExtension(item.name)});
     }
 
     const openItem = (item:Metadata) => {
       if(item.isDir){
-          goto(`${folderUri}/${item.name}`);
+          let prefix = folderUri.endsWith('/')?folderUri:(folderUri+'/');
+          goto(`${prefix}${item.name}`);
       }else{
-          dispatch('item-open',item);
+          dispatch('item-open',{...item,extension:parseFileExtension(item.name)});
       }
     }
 
@@ -39,31 +46,61 @@
 
 <div class={classes}>
     <Toolbar>
-        <a href={rootUri+'top'}>{rootText}</a>
-        {#each paths as path,idx}
-            <span> / </span>
-            {#if idx == paths.length-1}
-                <span>{path}</span>
-            {:else}
-                <a href={rootUri+path}>{path}</a>
-            {/if}
-        {/each}
+        <slot name="buttons"></slot>
+        <div class="file-path">
+            <a class="file-part-path" href={rootUri+'top'}>{rootText}</a>
+            {#each paths as path,idx}
+                <span> / </span>
+                {#if idx == paths.length-1}
+                    <span>{path}</span>
+                {:else}
+                    <a class="file-part-path" href={rootUri+path}>{path}</a>
+                {/if}
+            {/each}
+        </div>
     </Toolbar>
-    <div class="folder-content container-auto-height">
-        {#each metadatas as metadata}
+    <div class="folder-content flex-full" bind:this={container}>
+        {#each metadatas as metadata,index}
             <div class="folder-item" class:active={selectedIds.includes(metadata.name)}
-                 on:click={clickItem(metadata)}
+                 on:mousedown|stopPropagation={selectItem(metadata)}
                  on:dblclick={openItem(metadata)}>
-                <span class="item-icon"><Icon scale={3} data={metadata.isDir?folderIcon:listIcon}></Icon></span>
-                <span class="item-text">{metadata.name}</span>
+                <span class="item-icon">
+                    <Icon scale={3} data={metadata.isDir?folderIcon:(icons(metadata)||listIcon)}></Icon>
+                </span>
+                {#if renamingFile === metadata.name}
+                    <Input on:blur={(e)=>dispatch('item-rename',{isDir:metadata.isDir,name:metadata.name,newName:e.target.value,index,extension:parseFileExtension(metadata.name)})}
+                           on:click={(e)=>{e.stopPropagation()}}
+                           on:dblclick={(e)=>e.stopPropagation()}
+                           placeholder="请输入名称"
+                           class="item-text" title={metadata.name}
+                           value={metadata.isDir?metadata.name:trimFileExtension(metadata.name)}/>
+                {:else}
+                    <span class="item-text" title={metadata.name}>{metadata.isDir?metadata.name:trimFileExtension(metadata.name)}</span>
+                {/if}
+
             </div>
         {/each}
     </div>
 </div>
 
+<ContextMenu target={container} on:open={({detail})=>{dispatch('open-contextmenu',detail)}}>
+    <slot name="contextmenu">
+
+    </slot>
+</ContextMenu>
+
 <style lang="scss">
+  .file-path{
+    line-height: 1.8rem;
+    display: inline-block;
+  }
+  .file-part-path{
+    text-decoration: none;
+    color: blue;
+  }
   .folder-content {
     padding:6px;
+    background: #fdfdfd;
     .folder-item {
       float: left;
       width: 100px;
@@ -82,6 +119,8 @@
         text-align: center;
         display: inline-block;
         color: #d7a872;
+        line-height: 48px;
+        height: 50px;
       }
 
       .item-text {
